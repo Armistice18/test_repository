e8e4d0ec8e65a6d66a41b05042060a165d34967e
AKELE Armistice Epiphane <akelearmistice153@gmail.com>
2024-11-14 16:25:44 +0000

    testcommit

--- src/App.java
+++ src/App.java
@@ -1,29 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        int age = 15;
-        float taille = 1.82f;
-        char sexe = 'M';
-        boolean hasJob = true;
-        String nom = "AKELE";
-        String prenom = "Armistice";
-
-        System.out.println(hasJob);
-        System.out.println(age);
-        System.out.println(taille);
-        System.out.println(sexe);
-        System.out.println(hasJob);
-        System.out.println(nom);
-        System.out.println(prenom);
-
-        System.out.println("Bonjour" + nom + prenom + "votre age est" + age );
-        String situationProfessionnelle = hasJob ? "En emploi" : " Au chomage";
-      
-
-
-
-
-
-        
+        System.out.println("Hello, World!");  
     }
 }
